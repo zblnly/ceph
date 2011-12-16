72504abbba5c2f85e685edd5315888326668e73f
Samuel Just <samuel.just@dreamhost.com>
2011-12-16 11:22:58 -0800

    ReplicatedPG: add asserts to catch scrub error
    If last_update_applied skipped over last_update, we would see
    scrub hang.
    
    Signed-off-by: Samuel Just <samuel.just@dreamhost.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -2689,6 +2689,8 @@ void ReplicatedPG::op_applied(RepGather *repop)
   put_object_contexts(repop->src_obc);
   repop->obc = 0;
 
+  assert(info.last_update >= repop->v);
+  assert(last_update_applied < repop->v);
   last_update_applied = repop->v;
   if (last_update_applied == info.last_update && finalizing_scrub) {
     dout(10) << "requeueing scrub for cleanup" << dendl;
@@ -3433,6 +3435,8 @@ void ReplicatedPG::sub_op_modify_applied(RepModify *rm)
   rm->applied = true;
   bool done = rm->applied && rm->committed;
 
+  assert(info.last_update >= rm->op->version);
+  assert(last_update_applied < rm->op->version);
   last_update_applied = rm->op->version;
   if (last_update_applied == info.last_update && finalizing_scrub) {
     assert(active_rep_scrub);
