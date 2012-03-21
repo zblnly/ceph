6cbfcff6e6bbd40dec938d166ab0c17a2fb5219f
Greg Farnum <gregory.farnum@dreamhost.com>
2012-03-21 13:07:05 -0700

    test: add a StateAlert class to the TestDriver interface
    This lets us do async notifications.
    
    Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

--- src/test/msgr/TestDriver.h
+++ src/test/msgr/TestDriver.h
@@ -11,10 +11,13 @@
 #define TESTDRIVER_H_
 
 #include <set>
+#include <pthread.h>
 
 #include "msg/Messenger.h"
 #include "common/Mutex.h"
+#include "common/Cond.h"
 #include "common/code_environment.h"
+#include "common/StateTracker.h"
 
 class MessengerDriver;
 typedef std::tr1::shared_ptr<MessengerDriver> MDriver;
@@ -78,5 +81,49 @@ protected:
    * @} Orders
    */
 };
+
+/**
+ * The StateAlertImpl (which you should always reference as a shared_ptr, which
+ * is typedefed to StateAlert) specifies a state to alert on, provides a Cond
+ * to wait and alert on, and exposes a void *payload.
+ *
+ * The object reporting the state (probably a MessengerDriver) should call
+ * set_state_reached exactly once, and if it has a payload, pass it there.
+ * The object watching state should only look at payload once is_state_reached()
+ * returns true.
+ * Use a loop running on is_state_reached() and waiting on the Cond to wait until
+ * the StateAlert has been filled in.
+ *
+ * LATER: extend it briefly so that we can force the MessengerDriver to wait
+ * for permission from the TestDriver to continue running?
+ */
+class StateAlertImpl {
+  const StateTrackerImpl::State *state;
+  bool state_reached;
+  void *payload;
+  Mutex& lock;
+public:
+  Cond cond;
+
+  StateAlertImpl(const StateTrackerImpl::State *s, Mutex &_lock) : state(s),
+      state_reached(0), lock(_lock)
+  {}
+  ~StateAlertImpl() {}
+
+  const StateTrackerImpl::State *get_watched_state() { return state; }
+  void set_state_reached(void *payload=NULL) {
+    lock.Lock();
+    state_reached = true;
+    this->payload = payload;
+    cond.SignalAll();
+    lock.Unlock();
+  }
+  // hold the lock when calling this function
+  bool is_state_reached() {
+    return state_reached;
+  }
+  void *get_payload() { return payload; }
+};
+typedef std::tr1::shared_ptr<StateAlertImpl> StateAlert;
 
 #endif /* TESTDRIVER_H_ */
